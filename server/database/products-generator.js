5cf133a20c44dcae528c0b9fa2af3155e070e0b5
Torstein Molland <rex.datamaskin@gmail.com>
2017-10-31 14:47:51 +0100

    :leaves: Added database generation code

---
+++ server/database/products-generator.js
@@ -0,0 +1,14 @@
+// https://next.json-generator.com/V1MymexR7
+[
+  {
+    'repeat(40)': {
+      _id: '{{objectId()}}',
+      createdAt: '{{moment(this.date(new Date(2014, 0, 1), new Date())).unix()}}',
+      descripiton: '{{lorem(1, "paragraphs")}}',
+      name: '{{lorem(2, "words")}}',
+      image: 'http://via.placeholder.com/{{random(300,350,400)}}x{{random(200,300,350)}}',
+      price: '{{integer(300,20000)}}',
+      userId: ''
+    }
+  }
+]
