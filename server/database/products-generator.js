97af289690f98bf554887f2eafbcdb73de023759
Balázs <info@balazsorban.com>
2017-11-08 15:17:05 +0100

    :pencil: Fix typo.

--- server/database/products-generator.js
+++ server/database/products-generator.js
@@ -4,7 +4,7 @@
     'repeat(40)': {
       _id: '{{objectId()}}',
       createdAt: '{{moment(this.date(new Date(2014, 0, 1), new Date())).unix()}}',
-      descripiton: '{{lorem(1, "paragraphs")}}',
+      description: '{{lorem(1, "paragraphs")}}',
       name: '{{lorem(2, "words")}}',
       image: 'http://via.placeholder.com/{{random(300,350,400)}}x{{random(200,300,350)}}',
       price: '{{integer(300,20000)}}',
