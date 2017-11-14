1e1d88580e75acfb9c73a6de91d03acaf135d3a7
Torstein Molland <rex.datamaskin@gmail.com>
2017-11-14 14:39:05 +0100

    :umbrella: ProductsComponent tests running

--- client/app/products/products.component.spec.ts
+++ client/app/products/products.component.spec.ts
@@ -1,4 +1,64 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
+import { ProductsComponent } from './products.component'
+import { ProductDetailsComponent } from '../product-details/product-details.component'
+import { ToastComponent } from '../shared/toast/toast.component';
+
+
+import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service'
+import { MessageService } from '../services/message.service'
+import { AuthService } from '../services/auth.service'
+
+import { HttpModule } from '@angular/http'
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
+
+
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpModule, RouterTestingModule, FormsModule, ReactiveFormsModule ],
+      declarations: [ ProductsComponent ],
+      providers: [
+        UserService,
+        ProductService,
+        MessageService,
+        AuthService,
+        ProductDetailsComponent,
+        ToastComponent
+       ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+
+
+
+
+
+
+
+
 
 
 /*describe('Component: Cats', () => {
