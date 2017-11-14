fc04fd6b9d49f01f7eb44880524d291ed292ff33
Torstein Molland <rex.datamaskin@gmail.com>
2017-11-14 12:57:19 +0100

    :umbrella:

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -55,15 +55,17 @@ describe('Component: App', () => {
     authService.loggedIn = true;
     fixture.detectChanges();
     const de = fixture.debugElement.queryAll(By.css('a'));
-    expect(de.length).toBe(4);
+    expect(de.length).toBe(5);
     expect(de[0].nativeElement.textContent).toContain('Home');
     expect(de[1].nativeElement.textContent).toContain('Products');
     expect(de[2].nativeElement.textContent).toContain('Account (Tester)');
-    expect(de[3].nativeElement.textContent).toContain('Logout');
+    expect(de[3].nativeElement.textContent).toContain('My products');
+    expect(de[4].nativeElement.textContent).toContain('Logout');
     expect(de[0].attributes['routerLink']).toBe('/');
     expect(de[1].attributes['routerLink']).toBe('/products');
     expect(de[2].attributes['routerLink']).toBe('/account');
-    expect(de[3].attributes['routerLink']).toBe('/logout');
+    expect(de[3].attributes['routerLink']).toBe('/myProducts');
+    expect(de[4].attributes['routerLink']).toBe('/logout');
   });
 
   it('should display the navigation bar correctly for admin users', () => {
@@ -71,17 +73,19 @@ describe('Component: App', () => {
     authService.isAdmin = true;
     fixture.detectChanges();
     const de = fixture.debugElement.queryAll(By.css('a'));
-    expect(de.length).toBe(5);
+    expect(de.length).toBe(6);
     expect(de[0].nativeElement.textContent).toContain('Home');
     expect(de[1].nativeElement.textContent).toContain('Cats');
     expect(de[2].nativeElement.textContent).toContain('Account (Tester)');
     expect(de[3].nativeElement.textContent).toContain('Admin');
-    expect(de[4].nativeElement.textContent).toContain('Logout');
+    expect(de[4].nativeElement.textContent).toContain('My products');
+    expect(de[5].nativeElement.textContent).toContain('Logout');
     expect(de[0].attributes['routerLink']).toBe('/');
     expect(de[1].attributes['routerLink']).toBe('/products');
     expect(de[2].attributes['routerLink']).toBe('/account');
     expect(de[3].attributes['routerLink']).toBe('/admin');
-    expect(de[4].attributes['routerLink']).toBe('/logout');
+    expect(de[4].attributes['routerLink']).toBe('/myProducts');
+    expect(de[5].attributes['routerLink']).toBe('/logout');
   });
 
 });
