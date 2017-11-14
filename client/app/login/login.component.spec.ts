3a352ca7dfcf61b0a66c44dc6f58abd1c51374a5
Torstein Molland <rex.datamaskin@gmail.com>
2017-11-14 14:13:14 +0100

    :umbrella: LoginComponent tests running

--- client/app/login/login.component.spec.ts
+++ client/app/login/login.component.spec.ts
@@ -1,7 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { HttpModule } from '@angular/http'
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms'
+
+
+import { ToastComponent } from '../shared/toast/toast.component';
 import { LoginComponent } from './login.component';
+import { AuthService } from '../services/auth.service'
+import { UserService } from '../services/user.service'
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,7 +18,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [HttpModule, RouterTestingModule, ReactiveFormsModule],
+      declarations: [ LoginComponent ],
+      providers: [AuthService, UserService, ToastComponent],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -20,12 +32,12 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  /*it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should display the string "Login" in h4', () => {
     const el = fixture.debugElement.query(By.css('h4')).nativeElement;
     expect(el.textContent).toContain('Login');
-  });*/
+  });
 });
