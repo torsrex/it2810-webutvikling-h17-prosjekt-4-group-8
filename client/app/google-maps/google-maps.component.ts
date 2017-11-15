9e6deb5611c52bf704f06a4d178db0c9c26491a2
Balázs <info@balazsorban.com>
2017-11-15 22:24:21 +0100

    Show markers on the map for each user.

--- client/app/google-maps/google-maps.component.ts
+++ client/app/google-maps/google-maps.component.ts
@@ -11,39 +11,14 @@ export class GoogleMapsComponent implements OnInit {
 
   init_lat = 63.428024;
   init_lng = 10.393186;
-  zoom = 10;
+  zoom = 4;
   users = [];
-  locations = [];
-  isLoading = true;
-
-  // TODO: remove this, used for testing as no data currently has location
-  testUser = {
-    _id: 1,
-    username: "Person 1",
-    location: {
-      latitude: 63.42,
-      longitude: 10.37
-    },
-    products: ["jd7464djf484y7", "7f78f9shf"]
-  };
-  testBruker = {
-    _id: 2,
-    username: "Person 2",
-    location: {
-      latitude: 63.44,
-      longitude: 10.47
-    },
-    products: ["987fyd8fd7gf", "jiej9ijf9ie"]
-  };
-  testUsers = [];
+  isLoading = true
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.getUsers();
-    // TODO: remove these, used for testing as no data currently has locations
-    this.testUsers.push(this.testUser);
-    this.testUsers.push(this.testBruker);
   }
 
   filterByUser(id) {
@@ -53,24 +28,9 @@ export class GoogleMapsComponent implements OnInit {
 
   getUsers() {
     this.userService.getUsers().subscribe(
-      data => {
-        this.users = data
-      },
+      users => {this.users = users},
       error => console.log(error),
-      () => {
-        this.getLocations();
-      }
+      () => {this.isLoading = false}
     );
   }
-
-  getLocations() {
-    for (let i = 0; i < this.users.length; i++) {
-      const loc = this.users[i].location
-      if (loc) {
-        this.locations.push(this.users[i].location)
-      }
-    }
-    this.isLoading = false
-  }
-
 }
