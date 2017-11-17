f99c5db04f7b7eb5d83212e14081a520ca53c831
Torstein Molland <rex.datamaskin@gmail.com>
2017-11-17 17:41:29 +0100

    :umbrella: Added AdminComponent tests

--- client/app/admin/admin.component.spec.ts
+++ client/app/admin/admin.component.spec.ts
@@ -38,15 +38,57 @@ describe('AdminComponent', () => {
       fixture = TestBed.createComponent(AdminComponent);
       component = fixture.componentInstance;
       authService = fixture.debugElement.injector.get(AuthService);
+      component.users = [
+        {
+          _id: "5a0cb6756a60402154b7c724",
+          username: "PositionTest",
+          email: "positiontest@gmail.com",
+          phone: "99288571",
+          role: "admin",
+          latitude: 60,
+          longitude: 4,
+          __v: 0,
+          products: [ ]
+        },
+        {
+          _id: "5a0ca7fc6a60402154b7c723",
+          username: "posTest",
+          email: "postest@gmail.com",
+          phone: "99288571",
+          role: "admin",
+          latitude: 60.4344545,
+          longitude: 10.456765,
+          __v: 0,
+          products: [ ]
+        }
+      ]
       fixture.detectChanges();
     });
   }));
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('Should not have any users', () => {
-    let el = fixture.componentInstance
-    el.getUsers()
-    expect(el.users.length).toBe(0)
+  it('Should display number of users', () => {
+    component.isLoading = false
+    fixture.detectChanges()
+    const el = fixture.debugElement.query(By.css("h4"))
+    expect(el.nativeElement.textContent).toContain("Registered users (2)")
+  })
+  it('Should display user details', () => {
+    component.isLoading = false
+    fixture.detectChanges()
+    const el = fixture.debugElement.queryAll(By.css(".card-block table tbody tr td"))
+    expect(el.length).toBe(8)
+    expect(el[0].nativeElement.textContent).toContain("PositionTest")
+    expect(el[1].nativeElement.textContent).toContain("positiontest@gmail.com")
+    expect(el[2].nativeElement.textContent).toContain("admin")
+  })
+  it('Should call delete user function', () => {
+    component.isLoading = false
+    spyOn(component, 'deleteUser')
+    fixture.detectChanges()
+    const el = fixture.debugElement.query(By.css("button"))
+    el.triggerEventHandler('click', null)
+    expect(component.deleteUser).toHaveBeenCalled()
   })
 });
