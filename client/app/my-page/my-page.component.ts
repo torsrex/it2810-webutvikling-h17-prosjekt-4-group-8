d480f7005934310f88c53e2781e24e2e206cb653
Balázs <info@balazsorban.com>
2017-11-17 13:31:15 +0100

    Add address picket to register and mypage.

--- client/app/my-page/my-page.component.ts
+++ client/app/my-page/my-page.component.ts
@@ -2,20 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { ToastComponent } from '../shared/toast/toast.component';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
+import { MessageService } from '../services/message.service';
+import { Subscription } from 'rxjs/Subscription';
 import { MyProductsComponent } from '../my-products/my-products.component';
 
 @Component({
   selector: 'app-account',
-  templateUrl: './my-page.component.html'
+  templateUrl: './my-page.component.html',
+  styleUrls: ['./my-page.component.scss']
+
 })
 export class MyPageComponent implements OnInit {
   //TODO: Add validation just like in register page
-  user = {};
+  user = {
+    latitude: 0,
+    longitude: 0
+  };
   isLoading = true;
+  subscription: Subscription;
+  init_lat = 63.428024;
+  init_lng = 10.393186;
+  zoom = 4;
 
   constructor(private auth: AuthService,
               public toast: ToastComponent,
-              private userService: UserService) { }
+              private userService: UserService,
+              private messageService: MessageService) {
+                  this.subscription = this.messageService.getMessage().subscribe(msg => { this.init_lat = msg.text[0]; this.init_lng = msg.text[1]; this.zoom = 10; });
+    }
 
   ngOnInit() {
     this.getUser();
@@ -23,7 +37,9 @@ export class MyPageComponent implements OnInit {
 
   getUser() {
     this.userService.getUser(this.auth.currentUser).subscribe(
-      data => this.user = data,
+      data => {
+        this.user = data
+      },
       error => console.log(error),
       () => this.isLoading = false
     );
@@ -34,6 +50,18 @@ export class MyPageComponent implements OnInit {
       res => this.toast.setMessage('account settings saved!', 'success'),
       error => console.log(error)
     );
+  }
+
+  mapClick = ({coords: {lat: latitude, lng: longitude}}) => {
+    this.user = {
+      ...this.user,
+      latitude,
+      longitude
+    }
+  }
+
+  updateMap($event) {
+    this.zoom = $event;
   }
 
 }
